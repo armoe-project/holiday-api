6c3d3fda1c8fb30ca063468eb1564c03a04bb36a
真心 <qgzhenxin@qq.com>
2024-01-17 10:20:35 +0800

    feat: 每日执行定时任务

--- internal/cron.go
+++ internal/cron.go
@@ -8,13 +8,14 @@ func InitCron() {
 
 	// 创建定时任务
 	c := cron.New()
-	// 每月 25 日 01:00 执行
-	_, err := c.AddFunc("0 0 1 25 * *", syncHoliday)
+	// 每日 00:00:00 执行一次
+	_, err := c.AddFunc("@daily", syncHoliday)
 	if err != nil {
 		// 如果添加失败，则不继续执行
 		insertLog("添加定时任务失败, Error: " + err.Error())
 		return
 	}
+
 	// 启动定时任务
 	c.Start()
 }
